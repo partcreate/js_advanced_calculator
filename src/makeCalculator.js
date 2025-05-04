6754f7124a47bd05f76ad170357f187f8671fe32
PArtCreate <143039892+partcreate@users.noreply.github.com>
2025-05-04 04:25:24 +0000

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,48 @@
  */
 function makeCalculator() {
   // write code here
+
+  const calculator = {};
+
+  calculator.result = 0;
+
+  calculator.operate = function (operation, num) {
+    operation(num);
+
+    return this;
+  };
+
+  calculator.add = function (num) {
+    calculator.result += num;
+
+    return this;
+  };
+
+  calculator.subtract = function (num) {
+    calculator.result -= num;
+
+    return this;
+  };
+
+  calculator.multiply = function (num) {
+    calculator.result *= num;
+
+    return this;
+  };
+
+  calculator.divide = function (num) {
+    calculator.result /= num;
+
+    return this;
+  };
+
+  calculator.reset = function () {
+    calculator.result = 0;
+
+    return this;
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
